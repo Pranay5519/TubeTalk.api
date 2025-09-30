a411f16fc25fe6be6779b8a7c27dc5026aacaf95
Pranay Gundu <pranaygundu423@gmail.com>
2025-09-30 10:57:11 +0530

    added Middleware

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from app.api import topics,summary,quiz,chatbot
 # uvicorn app.main:app --reload
-
+import logging
+from app.middleware.logging_middleware import LoggingMiddleware
 app = FastAPI(
     title="TubeTalk.ai API",
     description="API for Quiz Generator, Summary Generator, RAG Chatbot, and Topics Generator",
     version="1.0.0"
 )
+#linking middleware to app
+logging.basicConfig(
+    level=logging.INFO,  # Set minimum level to INFO
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+app.add_middleware(LoggingMiddleware)
+
 app.include_router(topics.router,tags=["topics"])
 app.include_router(summary.router,tags=["summary"])
 app.include_router(quiz.router,tags=["quiz"])
