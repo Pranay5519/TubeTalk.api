c79ef990236768c189bb0176dbdf4243dde770e6
Pranay Gundu <pranaygundu423@gmail.com>
2025-10-27 09:55:02 +0530

    used webProxy for load_trranscritsFunction

--- app/utils/utility_functions.py
+++ app/utils/utility_functions.py
@@ -1,9 +1,8 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import re
-def load_transcript(url: str) -> str | None:
-    """
-    Fetch transcript for a YouTube video.
-    """
+from youtube_transcript_api.proxies import WebshareProxyConfig
+"""def load_transcript(url: str) -> str | None:
+ 
     pattern = r'(?:v=|\/)([0-9A-Za-z_-]{11})'
     match = re.search(pattern, url)
     if match:
@@ -15,3 +14,22 @@ def load_transcript(url: str) -> str | None:
         except Exception as e:
             print(f"❌ Error fetching transcript: {e}")
             return None
+"""        
+def load_transcript(url : str) -> str | None:
+    pattern = r'(?:v=|\/)([0-9A-Za-z_-]{11})'
+    match = re.search(pattern, url)
+    if match:
+        video_id = match.group(1)
+        try:
+            captions = YouTubeTranscriptApi( proxy_config=WebshareProxyConfig(
+        proxy_username="uovoumor",
+        proxy_password="udk28jufv40l",
+    )).fetch(video_id,languages=['en','hi']).snippets
+            data = [f"{item.text} ({item.start})" for item in captions]
+            return " ".join(data)
+        except Exception as e:
+            print(f"❌ Error fetching transcript: {e}")
+            return None
+    else:
+        print("❌ Invalid YouTube URL")
+        return None
--- test_api.py
+++ test_api.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+"""
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.proxies import WebshareProxyConfig
 import os
@@ -21,18 +21,10 @@ ytt_api = YouTubeTranscriptApi(proxy_config=proxy_config)
 # ✅ Fetch transcript
 transcript = ytt_api.fetch("s3KnSb9b4Pk",languages = ['en','hi']).snippets
 print(transcript)
-=======
-import os
-import redis
-from dotenv import load_dotenv
 
-load_dotenv()
-REDIS_URL = os.getenv("REDIS_URL")
+"""
 
-try:
-    client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
-    if client.ping():
-        print("✅ Redis URL is working!")
-except redis.ConnectionError as e:
-    print("❌ Redis connection failed:", e)
->>>>>>> 16b7fdcd96f11a6246e0c571b6a3be8fb7e999bf
+from app.utils.utility_functions import load_transcript
+
+transcritps = load_transcript('https://youtu.be/HcA3YIWoM0w')
+print(transcritps)
